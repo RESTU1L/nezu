9a4596eb5a44bbaa87cc4353f88d6ab3a6a52a68
RESTU1L <93248075+RESTU1L@users.noreply.github.com>
2022-04-05 21:44:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
+global.owner = ['6289696163176','0']
 global.packname = 'Bot Whatsapp'
 global.author = 'Nezuko'
 global.sessionName = 'data'
